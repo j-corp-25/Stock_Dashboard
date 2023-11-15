b765e27a8b798f6f8f618cb7b05aaac9d5f8e6b6
Jordy <jordycorporan95@gmail.com>
2023-11-15 13:22:48 -0500

    add error handling

--- backend_v2/config/routes.rb
+++ backend_v2/config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
   end
-  # post 'api/test', to: 'application#test'
+  post 'api/test', to: 'application#test'
 end
