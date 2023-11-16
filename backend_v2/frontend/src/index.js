b1d2fc6a9146422470a019f920bc6a386cbeb497
Jordy <jordycorporan95@gmail.com>
2023-11-15 20:19:31 -0500

    frontend auth set up

--- backend_v2/frontend/src/index.js
+++ backend_v2/frontend/src/index.js
@@ -5,19 +5,26 @@ import App from "./App";
 import store from "./store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import csrfFetch, { restoreCSRF } from "./store/csrf";
 
-
-
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
+  window.csrfFetch = csrfFetch;
 }
 
+const renderApp = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById("root")
+  );
+};
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApp);
+} else {
+  renderApp();
+}
