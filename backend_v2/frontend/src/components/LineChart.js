0cd351db4151a369c33db7be420988364f6d7276
Jordy <jordycorporan95@gmail.com>
2023-11-20 23:28:19 -0500

    changing chart stuff:

--- backend_v2/frontend/src/components/LineChart.js
+++ backend_v2/frontend/src/components/LineChart.js
@@ -1,79 +1,88 @@
-import axios from 'axios';
-import { Line } from 'react-chartjs-2';
-import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { Line } from "react-chartjs-2";
+import React, { useState, useEffect } from "react";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  } from 'chart.js';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-  );
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-const LineChart = () => {
+const LineChart = ({symbol="IBM"}) => {
   const [chartData, setChartData] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://www.alphavantage.co/query', {
+        const response = await axios.get("https://www.alphavantage.co/query", {
           params: {
-            function: 'TIME_SERIES_INTRADAY',
-            symbol: 'IBM',
-            interval: '5min',
-            apikey: 'demo' // Replace with your actual API key
-          }
+            function: "TIME_SERIES_INTRADAY",
+            symbol: symbol,
+            interval: "5min",
+            apikey: "demo", // Replace with your actual API key
+          },
         });
         // Process the data to the format suitable for Chart.js
-        const processedData = processData(response.data);
+        const processedData = processData(response.data,symbol);
         setChartData(processedData);
       } catch (error) {
-        console.error('Error fetching stock data:', error);
+        console.error("Error fetching stock data:", error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [symbol]);
 
-  const processData = (data) => {
-    const timeSeries = data['Time Series (5min)'];
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+    },
+  };
+
+  const processData = (data,symbol) => {
+    const timeSeries = data["Time Series (5min)"];
     const chartLabels = [];
     const chartData = [];
 
     for (let [key, value] of Object.entries(timeSeries)) {
       chartLabels.push(key);
-      chartData.push(value['4. close']);
+      chartData.push(value["4. close"]);
     }
 
     return {
       labels: chartLabels.reverse(),
       datasets: [
         {
-          label: 'Stock Price',
+          label: `${symbol} Price`,
           data: chartData.reverse(),
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
-        }
-      ]
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.1,
+        },
+      ],
     };
   };
 
   return (
     <div>
       {chartData && chartData.datasets ? (
-        <Line data={chartData} options={{ /* options for the chart */ }} />
+        <Line height={400} width={600} data={chartData} options={options} />
       ) : (
         <p>Loading...</p>
       )}
